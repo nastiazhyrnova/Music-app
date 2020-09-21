1ce446166f3563f0f579ec80e081fc027c2360ce
Ana <64069789+nas9lug2z@users.noreply.github.com>
2020-09-21 21:15:51 +0200

    export of music list with JS into the page  completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-// import songList from "./data/songs.js";
+import {songsList} from "./data/songs.js";
+
+//PLAYLIST: MUSIC INSERT
+const playListEl = document.querySelector(".song-list");
+
+
+for (let i = 0; i < songsList.length; i++) {
+    addSongToPlaylist(i);
+}
+
+
+
+
+
+//create a new li item
+function addSongToPlaylist (songArrId) {
+    const newLiEl = document.createElement("li");
+    newLiEl.classList.add("song-table");
+    newLiEl.classList.add("song-list");
+    const addHtml = `<span class="song-number">${songArrId+1}</span>
+                            <img src="${songsList[songArrId].cover}" alt="" class="song-cover-img">
+                            <span class="song-title">
+                                <span class="song-name">${songsList[songArrId].title}</span>
+                                <span class="song-author">${songsList[songArrId].artist}</span>
+                            </span>
+                            <span class="song-album">Best Album Ever</span>
+                            <span class="song-add-to-fav"><i class="far fa-heart"></i></span>
+                            <span class="song-length">${songsList[songArrId].time}</span>
+                            <span class="song-dropdown-menu-button"><i class="fas fa-ellipsis-h three-dots-icon"></i></span>`;
+    // console.log(newSongLine);
+    newLiEl.innerHTML = addHtml;
+    playListEl.appendChild(newLiEl);
+};
+
+
+// addSongToPlaylist(2);
+
--- js/data/songs.js
+++ js/data/songs.js
@@ -1,89 +1,89 @@
 export const songsList = [{
     "id": "1",
-    "url": "../../music/all-together.mp3",
+    "url": "music/all-together.mp3",
     "time": "01:39",
     "title": "All Together",
     "artist": "Famous Artist",
-    "cover": "../../images/1.jpg"
+    "cover": "images/1.jpg"
   },
   {
     "id": "2",
-    "url": "../../music/ambient-call.mp3",
+    "url": "music/ambient-call.mp3",
     "time": "03:29",
     "title": "Ambient Call of Motherland",
     "artist": "Not very famous Artist",
-    "cover": "../../images/2.jpg"
+    "cover": "images/2.jpg"
   },
   {
     "id": "3",
-    "url": "../../music/sport-music.mp3",
+    "url": "music/sport-music.mp3",
     "time": "02:07",
     "title": "Sport Music",
     "artist": "BearStockMusic",
-    "cover": "../../images/3.jpg"
+    "cover": "images/3.jpg"
   },
   {
     "id": "4",
-    "url": "../../music/perfect-day.mp3",
+    "url": "music/perfect-day.mp3",
     "time": "04:40",
     "title": "Perfect Day",
     "artist": "Happy Singer",
-    "cover": "../../images/4.jpg"
+    "cover": "images/4.jpg"
   },
   {
     "id": "5",
-    "url": "../../music/full-track.mp3",
+    "url": "music/full-track.mp3",
     "time": "02:25",
     "title": "Full Track",
     "artist": "Unknown Artist",
-    "cover": "../../images/5.jpg"
+    "cover": "images/5.jpg"
   },
   {
     "id": "6",
-    "url": "../../music/Powerful-Indie.mp3",
+    "url": "music/Powerful-Indie.mp3",
     "time": "01:52",
     "title": "Powerful Indie Rock full",
     "artist": "Power Artist",
-    "cover": "../../images/6.jpg"
+    "cover": "images/6.jpg"
   },
   {
     "id": "7",
-    "url": "../../music/summer-love.mp3",
+    "url": "music/summer-love.mp3",
     "time": "02:31",
     "title": "Summer Love",
     "artist": "Artist in Love",
-    "cover": "../../images/7.jpg"
+    "cover": "images/7.jpg"
   },
   {
     "id": "8",
-    "url": "../../music/tech-and-sport.mp3",
+    "url": "music/tech-and-sport.mp3",
     "time": "02:08",
     "title": "Tech and Sport Breakbeat",
     "artist": "Techno Artist",
-    "cover": "../../images/8.jpg"
+    "cover": "images/8.jpg"
   },
   {
     "id": "9",
-    "url": "../../music/The-Future-Bass.mp3",
+    "url": "music/The-Future-Bass.mp3",
     "time": "02:07",
     "title": "The Future Bass",
     "artist": "Upcoming Singer",
-    "cover": "../../images/9.jpg"
+    "cover": "images/9.jpg"
   },
   {
     "id": "10",
-    "url": "../../music/Wanna-Know-You.mp3",
+    "url": "music/Wanna-Know-You.mp3",
     "time": "02:28",
     "title": "Wanna Know you",
     "artist": "Mix Artist",
-    "cover": "../../images/10.jpg"
+    "cover": "images/10.jpg"
   },
   {
     "id": "11",
-    "url": "../../music/yuar.mp3",
+    "url": "music/yuar.mp3",
     "time": "02:48",
     "title": "Happy Commercial Indie Folk",
     "artist": "Yuar",
-    "cover": "../../images/11.jpg"
+    "cover": "images/11.jpg"
   }
 ];
