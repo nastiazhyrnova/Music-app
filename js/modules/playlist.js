9227a8296d93a0e5fd6f5149023742a264f9f20e
Ana <64069789+nas9lug2z@users.noreply.github.com>
2020-09-28 19:18:24 +0200

    player function: now song source is fetched by song ID (for future shuffle options)

--- js/modules/playlist.js
+++ js/modules/playlist.js
@@ -1,24 +1,38 @@
 import {songsList as songs, songsList} from "./../data/songs.js";
 
 export const PlaylistMainFunction =  ( _ => {
-    //---DECLARATIONS & DOM
+    //---DECLARATIONS & caching the DOM
 
     let currentPlayingIndex = 1;
-    let currentSong = new Audio(songs[currentPlayingIndex].url);
+    let currentSong = new Audio(songs[currentPlayingIndex-1].url);
     let isPlaying = false;
 
     const playListEl = document.querySelector(".song-list");
     const mainPlayButton = document.querySelector(".play-button");
-    const playIcons = document.querySelectorAll(".fa-play")
     const playerPlayButton = document.querySelector(".player-playpause-button");
+    const playIcons = document.querySelectorAll(".fa-play")
+
 
 
     //---AUXULIARY FUNCTIONS (OR REPEATED MORE THAN ONCE)
 
     //visual changes for an active song - highlighting etc
-    const highlightActiveSongElement = (songIndex, styleForActive) => {
-        if (songIndex === currentPlayingIndex) {
-            return styleForActive;
+    const highlightActiveSongElement = (clickedId) => {
+        const currentPlaylist = playListEl.children;
+        for (let j = 1; j < currentPlaylist.length; j++) {
+            if (j === clickedId) {
+                currentPlaylist[j].classList.add("song-active-row");
+                //get children elements of li item
+                const children = currentPlaylist[j].children;
+                children[1].classList.add("song-active");
+                children[3].firstElementChild.classList.add("song-active");
+            }
+            else {
+                currentPlaylist[j].classList.remove("song-active-row");
+                const children = currentPlaylist[j].children;
+                children[1].classList.remove("song-active");
+                children[3].firstElementChild.classList.remove("song-active");
+            }
         }
     }
 
@@ -41,8 +55,13 @@ export const PlaylistMainFunction =  ( _ => {
     }
 
     //change audio source of the current song
-    const changeURLofSong = _ => {
-        currentSong.src = songs[currentPlayingIndex].url;
+    const changeURLofSong = (songId) => {
+        for (let i = 0; i < songs.length; i++) {
+            if (songs[i].id == songId) {
+                currentSong.src = songs[i].url;
+            }
+        }
+        // currentSong.src = songs[currentPlayingIndex-1].url;
     }
 
 
@@ -51,22 +70,22 @@ export const PlaylistMainFunction =  ( _ => {
     //1. Insert music from the playlist
     const importAllSongs = _ => {
         //add all songs from the file + highligth the active song
-        let length = songs.length;
-        for (let i = 1; i <= length; i++) {
+        for (let i = 0; i < songs.length; i++) {
             //create a new li item function
             ( _ => {
                 const newLiEl = document.createElement("li");
                 newLiEl.classList.add("song-table");
                 newLiEl.classList.add("song-single");
-                const addHtml = `<span class="song-number ${highlightActiveSongElement(i, 'song-active')}">${i}</span>
-                                        <img src="${songs[i-1].cover}" alt="" class="song-cover-img">
+                const addHtml = `<span class="song-id">${songs[i].id}</span>
+                                    <span class="song-number">${i+1}</span>
+                                        <img src="${songs[i].cover}" alt="" class="song-cover-img">
                                         <span class="song-title">
-                                            <span class="song-name ${highlightActiveSongElement(i, 'song-active')}">${songs[i-1].title}</span>
-                                            <span class="song-author">${songs[i-1].artist}</span>
+                                            <span class="song-name">${songs[i].title}</span>
+                                            <span class="song-author">${songs[i].artist}</span>
                                         </span>
                                         <span class="song-album">Best Album Ever</span>
                                         <span class="song-add-to-fav"><i class="far fa-heart"></i></span>
-                                        <span class="song-length">${songs[i-1].time}</span>
+                                        <span class="song-length">${songs[i].time}</span>
                                         <span class="song-dropdown-menu-button"><i class="fas fa-ellipsis-h three-dots-icon"></i></span>`;
                 // console.log(newSongLine);
                 newLiEl.innerHTML = addHtml;
@@ -78,10 +97,15 @@ export const PlaylistMainFunction =  ( _ => {
 
     //2. Change current playing index to the index of the clicked song
     const getClickedSongIndex = (e) => {
-        for (let eachChild of playListEl.children) {
-            eachChild.addEventListener('click', e => {
-                const clickedIndexOfLi = [...eachChild.parentElement.children].indexOf(eachChild);
-                mainPlay(clickedIndexOfLi);
+        const currentPlaylist = playListEl.children;
+        for (let j = 1; j < currentPlaylist.length; j++) {
+            currentPlaylist[j].addEventListener('click', e => {
+                let liChildren = currentPlaylist[j].children;
+                const clickedNoOfSong = Number(liChildren[1].innerHTML);
+                const clickedIdOfSong = Number(liChildren[0].innerHTML);
+                changeURLofSong(clickedIdOfSong);
+                mainPlay(clickedNoOfSong);
+                highlightActiveSongElement(clickedNoOfSong);
             })
         }
     }
@@ -96,17 +120,20 @@ export const PlaylistMainFunction =  ( _ => {
             changeURLofSong();
             togglePlayPause();
         }
+
     }
 
     //???. Run all event listeners
     const listeners = _ => {
         mainPlayButton.addEventListener('click', (e) => {
             e.preventDefault();
-            togglePlayPause();
+            togglePlayPause()
+            highlightActiveSongElement(currentPlayingIndex);
         });
         playerPlayButton.addEventListener('click', (e) => {
             e.preventDefault();
             togglePlayPause();
+            highlightActiveSongElement(currentPlayingIndex);
         });
     }
 
